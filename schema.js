7764b2c33daadeaa01926fdf5429fd11a0b95d68
Dylan White <dylanawhite11@gmail.com>
2019-04-04 00:29:27 -0500

    expanded RootQuery to include all customers in hardcoded data

--- schema.js
+++ schema.js
@@ -58,6 +58,12 @@ const RootQuery = new GraphQLObjectType({
                     }
                 }
             }
+        },
+        customers : {
+            type : new GraphQLList(CustomerType),
+            resolve(parentValue, args){
+                return customers;
+            }
         }
     }
 });
